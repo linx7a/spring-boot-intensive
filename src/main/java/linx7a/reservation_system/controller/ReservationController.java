cd96343e9d1b330cf38f5968891069b4e3d26be5
Alina Liechtenstein <alinaliechtenstein7@gmail.com>
2026-09-09 09:29:42 +0300

    chore: add logging to ReservationController

--- src/main/java/linx7a/reservation_system/controller/ReservationController.java
+++ src/main/java/linx7a/reservation_system/controller/ReservationController.java
@@ -2,14 +2,16 @@ package linx7a.reservation_system.controller;
 
 import linx7a.reservation_system.model.Reservation;
 import linx7a.reservation_system.service.ReservationService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+
 @RestController
 public class ReservationController {
+    private static final Logger log = LoggerFactory.getLogger(ReservationController.class);
     private final ReservationService reservationService;
 
     public ReservationController(ReservationService reservationService) {
@@ -22,6 +24,7 @@ public class ReservationController {
     }
     @GetMapping("/{id}")
     public Reservation getReservationById(@PathVariable Long id) {
+        log.info("Вызван getReservationById: id=" + id);
         return reservationService.getReservationById(id);
     }
 }
