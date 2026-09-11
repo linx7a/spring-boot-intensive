f50cdb192cea3068c521cfca39260c5e55a4c77d
Alina Liechtenstein <alinaliechtenstein7@gmail.com>
2026-09-11 11:44:33 +0300

    fix: add @Enumerated(EnumType.STRING) to ReservationEntity status field

--- src/main/java/linx7a/reservation_system/entity/ReservationEntity.java
+++ src/main/java/linx7a/reservation_system/entity/ReservationEntity.java
@@ -19,6 +19,7 @@ public class ReservationEntity {
     private LocalDate startDate;
     @Column(name = "end_date")
     private LocalDate endDate;
+    @Enumerated(EnumType.STRING)
     @Column(name = "status")
     private ReservationStatus status;
 
